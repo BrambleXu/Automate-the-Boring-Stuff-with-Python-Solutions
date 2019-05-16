d8c57d9c06f0fa45b82f005edfdf9e427494deab
usalman <umit.salman.mail@gmail.com>
2019-05-17 02:14:55 +0300

    slightly different solution for quiz file project

--- 8_Reading_and_Writing_Files/Chapter_Project/randomQuizGenerator.py
+++ 8_Reading_and_Writing_Files/Chapter_Project/randomQuizGenerator.py
@@ -1,47 +1,79 @@
-#! python3
-# randomQuizGenerator.py - Creates quizzes with questions and answers in
-# random order, along with the answer key.
+"""
+In this solution we obtain 2 different folder:
+    1: quiz-papers folder that contains 35 different quizzes
+    2: quiz-answers folder that contains answer keys to the each quizzes
 
+Each quiz is different from the others in terms of question order and
+options' order of the each question
+"""
+from random import shuffle
 import random
+import os
 
-# The quiz data. Keys are states and values are their capitals.
-capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix', 'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver', 'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee', 'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas': 'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine': 'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan': 'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri': 'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada': 'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh', 'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City', 'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence', 'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee': 'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont': 'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
-capitalsItems = list(capitals.items())
+capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
+   'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
+   'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
+   'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois':
+   'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas':
+   'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine':
+   'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan':
+   'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri':
+   'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada':
+   'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton',
+   'New Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh',
+   'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
+   'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
+   'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee':
+   'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont':
+   'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia',
+   'West Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
 
-# Generate 35 quiz files.
-for quizNum in range(35): 
-    # Create the quiz and answer key files.
-    quizFile = open('capitalsquiz%s.txt' % (quizNum + 1), 'w')
-    answerKeyFile = open('capitalsquiz_answers%s.txt' % (quizNum + 1), 'w')
+# creating a 'quiz-papers' folder for question papers
+if not os.path.exists(os.getcwd() + '/quiz-papers'):
+    os.mkdir('quiz-papers')
+# creating a 'quiz-answers' folder contains answer keys for each question paper
+if not os.path.exists(os.getcwd() + '/quiz-answers'):
+    os.mkdir('quiz-answers')
 
-    # Write out the header for the quiz.
-    quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
-    quizFile.write((' ' * 20) + 'State Capitals Quiz (Form %s)' % (quizNum + 1))
-    quizFile.write('\n\n')
+quizFilesPath = os.getcwd() + '/quiz-papers'
+quizAnswersPath = os.getcwd() + '/quiz-answers'
+states = list(capitals.keys())
 
-    # Shuffle the order of the states.
-    states = list(capitals.keys()) # get all states in a list
-    random.shuffle(states) # randomize the order of the states
+for i in range(35):
 
-    # Loop through all 50 states, making a question for each.
-    for questionNum in range(50):
+    # creating question papers and adding header to each one of them
+    questionFile = open(quizFilesPath + '/quiz' + str(i + 1) + '.txt', 'w')
+    questionFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
+    questionFile.write((' ' * 20) + 'State Capitals Quiz (Form %s)' % (i + 1))
+    questionFile.write('\n\n')
+    
+    # creating answer key for each question paper
+    answerFile = open(quizAnswersPath + '/quiz' + str(i + 1) + 'answers.txt', 'w')
 
-        # Get right and wrong answers.
-        correctAnswer = capitals[states[questionNum]]
-        wrongAnswers = list(capitals.values()) # get a complete list of answers
-        del wrongAnswers[wrongAnswers.index(correctAnswer)] # remove the right answer
-        wrongAnswers = random.sample(wrongAnswers, 3) # pick 3 random ones
+    shuffle(states)
+    questionNumber = 1
 
-        answerOptions = wrongAnswers + [correctAnswer]
-        random.shuffle(answerOptions) # randomize the order of the answers
+    for state in states:        
+        questionFile.write('\n\nQ' + str(questionNumber) + ':\nWhat is the capital of the ' + str(state) + '?\n\n')
+        answerFile.write('\n\nQ' + str(questionNumber) + ':\n' + str(capitals[state]) + '\n\n')
+        questionNumber += 1
+        capitalList = list(capitals.values())
+        capitalList.remove(str(capitals[state]))
 
-        # Write the question and answer options to the quiz file.
-        quizFile.write('%s. What is the capital of %s?\n' % (questionNum + 1, states[questionNum]))
-        for i in range(4):
-            quizFile.write('    %s. %s\n' % ('ABCD'[i], answerOptions[i]))
-        quizFile.write('\n')
+        # adding correct answer to the options
+        choices = [str(capitals[state])]
 
-        # Write out the answer key to a file.
-        answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[answerOptions.index(correctAnswer)]))
-    quizFile.close()
-    answerKeyFile.close()
+        # adding 3 wrong answers to the options
+        for i in range(3):
+            randNum = random.randint(0, len(capitalList) - 1)
+            choices.append(capitalList[randNum])
+            del capitalList[randNum]
+
+        shuffle(choices)
+        for i in range(len(choices)):
+            questionFile.write(str(choices[i]) + '\t')
+
+        questionFile.write('\n')
+
+questionFile.close()
+answerFile.close()
