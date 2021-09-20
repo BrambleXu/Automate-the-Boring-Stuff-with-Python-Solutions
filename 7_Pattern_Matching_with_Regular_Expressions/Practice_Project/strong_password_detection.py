19d62d8042eaba6046dea087608ff8fda166206e
Jamal Makame <jamal.abdalla@students.jkuat.ac.ke>
2021-09-20 17:29:29 +0300

    Update strong_password_detection.py
    I have re-written the code in a simpler format.
    I have also integrated the use of 'Pipe' in the code which makes the code neater.

--- 7_Pattern_Matching_with_Regular_Expressions/Practice_Project/strong_password_detection.py
+++ 7_Pattern_Matching_with_Regular_Expressions/Practice_Project/strong_password_detection.py
@@ -7,9 +7,9 @@ and has at least one digit.
 
 You may need to test the string against multiple regex patterns to validate its strength.
 '''
-import re
+#import re
 
-def pw_detection(pw):
+'''def pw_detection(pw):
     if len(pw) < 8:
     	return False
     elif re.search('[0-9]', pw) is None:
@@ -27,3 +27,22 @@ if __name__ == '__main__':
         print('Good, your password is strong!')
     else:
         print ('The password is at least eight characters long, contains both uppercase and lowercase characters, and has at least one digit.')
+        '''
+import re
+def Pass_detection(pw):
+    if len(pw) < 8:
+       return False
+    elif re.search('[0-9]|[A-Z]|[a-z]', pw) is None:
+        return False
+    else:
+        return True
+
+pw = input('Enter your password : ')
+if Pass_detection(pw):
+    print('Good, your password is strong!')
+else:
+    print('''
+    The Password should at least be eight characters long,
+    contains both uppercase and lowercase characters,
+    and has at least one digit.
+    ''')
